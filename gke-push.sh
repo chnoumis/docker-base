6a0e59fc638e27921a3bc775cb899267919bfb3a
rajan <vrajan@chnoumis.com>
2017-07-18 20:07:59 +1000

    Add gcloud docker push

--- gke-push.sh
+++ gke-push.sh
@@ -4,4 +4,4 @@
 PROJECT_ID=$(curl -s 'http://metadata/computeMetadata/v1/project/project-id' -H 'Metadata-Flavor: Google')
 docker build -t ${PROJECT_NAME}:${PROJECT_VERSION} .
 docker tag -f ${PROJECT_NAME}:${PROJECT_VERSION} asia.gcr.io/${PROJECT_ID}/${PROJECT_NAME}:${PROJECT_VERSION}
-gcloud docker push asia.gcr.io/${PROJECT_ID}/${PROJECT_NAME}:${PROJECT_VERSION}
+gcloud docker -- push asia.gcr.io/${PROJECT_ID}/${PROJECT_NAME}:${PROJECT_VERSION}
